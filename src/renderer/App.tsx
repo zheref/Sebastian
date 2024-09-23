84b9b6f1602c6242de5671629af95694fd4fd801
Sergio Daniel <me@zheref.io>
2024-09-22 20:07:53 -0500

    WIP

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,23 +1,94 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
-import { BaseStyles, Button, ThemeProvider } from '@primer/react';
+import { BaseStyles, Box, Button, Heading, NavList, SplitPageLayout, Text, ThemeProvider } from '@primer/react';
 
-function Hello() {
+function CommandView() {
   return (
-    <Button>Click me</Button>
-  );
+    <>
+      <Heading
+        as="h2"
+        sx={{
+          fontSize: 4,
+          fontWeight: 'normal',
+          color: 'danger.fg',
+          mb: 2,
+        }}
+      >
+        Danger zone
+      </Heading>
+      <Box
+        sx={{
+          border: '1px solid',
+          borderColor: 'danger.emphasis',
+          borderRadius: 2,
+          p: 3,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          gap: 3,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'grid',
+            gap: 1,
+          }}
+        >
+          <Text
+            sx={{
+              fontSize: 1,
+              fontWeight: 'bold',
+              color: 'danger.fg',
+            }}
+          >
+            Delete account
+          </Text>
+          <Text
+            sx={{
+              fontSize: 1,
+              color: 'fg.muted',
+            }}
+          >
+            Are you sure you don&apos;t want to just downgrade your account to a
+            free account? We won&apos;t charge your credit card anymore.
+          </Text>
+        </Box>
+        <Button variant="danger">Delete account</Button>
+      </Box>
+    </>
+  )
+}
+
+function ContentView() {
+  return (
+    <SplitPageLayout>
+    <SplitPageLayout.Pane position="start">
+      <NavList aria-label="Main navigation">
+        <NavList.Item href="#">Profile</NavList.Item>
+        <NavList.Item href="#" aria-current="page">
+          Account
+        </NavList.Item>
+        <NavList.Item href="#">Emails</NavList.Item>
+        <NavList.Item href="#">Notifications</NavList.Item>
+      </NavList>
+    </SplitPageLayout.Pane>
+    <SplitPageLayout.Content>
+      <Router>
+          <Routes>
+            <Route path="/" element={<CommandView />} />
+          </Routes>
+        </Router>
+    </SplitPageLayout.Content>
+  </SplitPageLayout>
+  )
 }
 
 export default function App() {
   return (
     <ThemeProvider>
       <BaseStyles>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Hello />} />
-          </Routes>
-        </Router>
+        <ContentView />
       </BaseStyles>
     </ThemeProvider>
   )
